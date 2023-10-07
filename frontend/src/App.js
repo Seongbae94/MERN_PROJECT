6ed811ecdaea63087652c2a5e9b2db051480ffc6
Seongbae94 <seongbaelee944@gmail.com>
2023-10-07 16:08:30 +0900

    feat: working on navigationbar
    working on navigationbar (on-going)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} exact />
-        <Route path="/places/new" element={<NewPlace />} exact />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} exact />
+          <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
