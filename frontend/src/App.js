70cfe33a2e0abd6d0fadb1183883e431ff63ecb5
Seongbae94 <seongbaelee944@gmail.com>
2023-10-08 13:25:48 +0900

    feat: working on from to modify a place
    working on from to modify a place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Users />} exact />
           <Route path="/:userId/places" exact element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="/places/:placeId" element={<UpdatePlace />} exact />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
