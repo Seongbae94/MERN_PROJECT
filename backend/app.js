c7363acffa4d986ae2e77308c3199f814bb58712
Seongbae94 <seongbaelee944@gmail.com>
2023-10-09 23:21:22 +0900

    feat: add error handling based on valid routes
    add error handling based on valid routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
 
+//triggers when facing unknown routes
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+//default error handling middleware
 app.use((error, req, res, next) => {
   if (res.headersSent) {
     return next(error);
